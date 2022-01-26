9bc87c609aba8f596aa5a66d158d247bddf69096
Lead Developer <git@ssl7.net>
2022-01-26 17:15:55 +0100

    comments updated

--- examples/message.php
+++ examples/message.php
@@ -1,14 +1,12 @@
 <?php
 require_once('../PhpSIP.class.php');
 
-/* Sends NOTIFY to reset Linksys phone */
-
 try
 {
   $api = new PhpSIP('127.0.0.1', 5060);
   $api->setMethod('MESSAGE');
   $api->setFrom('sip:10000@127.0.0.1');
-  $api->setUri('sip:10000@192.168.5.65:5061');
+  $api->setUri('sip:10000@192.168.5.65:5061'); // <- CHANGE IP address here
   $api->setBody('Hello world');
   $res = $api->send();
 
